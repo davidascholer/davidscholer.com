6044438be59866ffe1dfee57ec680841ef3e9737
David Scholer <davidascholer@gmail.com>
2025-06-04 22:37:18 -0700

    google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./ThemeProvider";
+import Script from "next/script";
 
 const AncizarFont = localFont({
   src: "../assets/fonts/AncizarSerif-Medium.ttf",
@@ -22,6 +23,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-17J7L93QPM"
+        ></Script>
+        <Script id="google-analytics">
+          {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-17J7L93QPM');`}
+        </Script>
+      </head>
       <body className={`${AncizarFont.variable}`}>
         <ThemeProvider
           attribute="class"
